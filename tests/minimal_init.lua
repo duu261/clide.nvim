8ce2a53cae2d015c6fa6510110d0498214f0ae93
Duu <vuhn261@gmail.com>
2026-07-03 03:03:27 +0700

    fix(tests): disable swapfile in minimal_init

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,2 +1,4 @@
 vim.opt.runtimepath:prepend(vim.fn.getcwd())
 vim.opt.runtimepath:prepend(vim.fn.getcwd() .. "/.deps/plenary.nvim")
+-- specs run in parallel nvim processes and share fixtures; swapfiles collide (E325)
+vim.o.swapfile = false
